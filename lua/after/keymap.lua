9390744af41635e91abd963e8383814770013b7b
khanadat <khanadat@student.42tokyo.jp>
2026-06-18 13:35:49 +0900

    modify: syntax error

--- lua/after/keymap.lua
+++ lua/after/keymap.lua
@@ -2,4 +2,5 @@ vim.keymap.set('n', '<Space>w', ':w<CR>', { silent = true })
 vim.keymap.set('n', '<Space>wq', ':wq<CR>', { silent = true })
 vim.keymap.set('n', '<Space>-', ':Ex<CR>', { silent = true })
 vim.keymap.set('n', '<Space>ss', ':%s/', { silent = true })
-vim.keymap.set('t', '<C-\\><C-n>', { silent = true })
+
+vim.keymap.set('t', '<Esc>', '<C-\\><C-n>', { silent = true })
